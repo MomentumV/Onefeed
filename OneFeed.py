baa8e320e61bfffc2a8bea3b2a858bd22f85d0d4
MomentumV <momentumv@users.noreply.github.com>
2020-01-18 10:16:30 -0500

    Overload rss_file method to remove options and add main routine
    default rss file will have filename based on the feed title.
    main routine provides some working code for testing.

--- OneFeed.py
+++ OneFeed.py
@@ -9,24 +9,45 @@ from feedgen.feed import FeedGenerator
 # each "show" has its own object, that gets updater functions added to it
 class PodCast(FeedGenerator):
     def __init__(self):
+        """
+        This wrapper of FeedGenerator adds the podcast extension at creation.
+        """
         super().__init__()
         self.load_extension('podcast')
+    # TODO future: subclass Podcast to make an archiver one that also stores the files...
+    #   Then, make OnePlace RT a subclass of that downloader class.
 
-
-# noinspection PyUnresolvedReferences
 class OnePlacePodCast(PodCast):
     def __init__(self, page=None):
         super().__init__()
         self.link(href='http://oneplace.com', rel='alternate')
         self.language('en')
         self.podcast.itunes_category('Christianity')
-
-    self.pageUrl = page  # this is to be accessible and updated when this class is instanced
+        self.pageUrl = page  # this is to be accessible and updated when this class is instanced
 
     # the pageUrl should be the url to be retrieved during updating.
+    def rss_file(self, filename=None, pretty=True):
+        if filename is None:
+            filename = self.title() + ' rss.xml'
+        extensions = True
+        encoding = 'UTF-8'
+        xml_declaration = True
+        super().rss_file(filename, extensions, pretty, encoding, xml_declaration)
+
     def refresh(self):
         # TODO retrieve page and extract information
         pass
         return
 
-RadioTheatre = OnePlacePodCast('https://www.oneplace.com/ministries/focus-on-the-familys-radio-theatre/')
+def main():
+    page = 'https://www.oneplace.com/ministries/focus-on-the-familys-radio-theatre/'
+    RadioTheatre = OnePlacePodCast(page)
+    RadioTheatre.title('Radio Theatre')
+    RadioTheatre.description('The Weekly broadcast of the award winning audio dramas '
+                             'from Focus on the Family, hosted by OnePlace.com')
+    RadioTheatre.id(page)
+
+    return RadioTheatre
+
+if __name__ == '__main__':
+    main()
