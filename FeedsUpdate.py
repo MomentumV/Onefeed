8a756c165404f9ee8b0c52762c5a343952a2721f
MomentumV <momentumv@users.noreply.github.com>
2020-01-20 14:44:45 -0500

    update logging to include timestamp and append to file

--- FeedsUpdate.py
+++ FeedsUpdate.py
@@ -1,20 +1,22 @@
 
 import pickle
 import git_code
+from datetime import datetime
 with open('feeds.p', 'rb') as f:
     feeds = pickle.load(f)
 
 # call the refresh of each feed and then write the rss feed
-with open('update.log', 'w') as log:
+with open('update.log', 'a') as log:
     for feed in feeds:
         try:
             ep = feed.refresh()
         except:
-            log.write(f'exception updating {feed.title()}')
+            log.write(str(datetime.now()))
+            log.write(f':\texception updating {feed.title()}\n')
         if ep is None:
-            log.write(f'No new show for {feed.title()}')
+            log.write(f':\tNo new show for {feed.title()}\n')
         else:
-            log.write(f'{ep.title()} added to {feed.title()}')
+            log.write(f':\t{ep.title()} added to {feed.title()}')
             # write out the rss file if there is an episode added
             feed.rss_file()
 
