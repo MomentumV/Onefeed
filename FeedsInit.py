62616f9d780e15d6399dda04d1e4cf40db9c7839
MomentumV <momentumv@users.noreply.github.com>
2020-01-20 10:33:27 -0500

    Add author details to initilization of feeds

--- FeedsInit.py
+++ FeedsInit.py
@@ -7,22 +7,27 @@ page = 'https://www.oneplace.com/ministries/focus-on-the-familys-radio-theatre/'
 title = 'Radio Theatre'
 desc = 'The Weekly broadcast of the award winning audio dramas ' \
        'from Focus on the Family, hosted by OnePlace.com'
-id = page
-RT = OneFeed.OnePlacePodCast(page, title, desc, id)
+author_dict = {'name': 'Focus on the Family', 'email': 'help@FocusontheFamily.com'}
+feed_id = page
+RT = OneFeed.OnePlacePodCast(page, title, desc, feed_id)
+RT.author(author_dict)
 RT.set_download(True, 'E:\\Audio Theatre and Drama\\OnePlace - Radio Theatre')
 RT.refresh(
-    'https://www.oneplace.com/ministries/focus-on-the-familys-radio-theatre/listen/the-hiding-place-part-01-of-09-792544.html')
+    'https://www.oneplace.com/ministries/focus-on-the-familys-radio-theatre/listen/the-hiding-place-part-01-of-09'
+    '-792544.html')
 RT.refresh(
-    'https://www.oneplace.com/ministries/focus-on-the-familys-radio-theatre/listen/the-hiding-place-part-02-of-09-792545.html')
+    'https://www.oneplace.com/ministries/focus-on-the-familys-radio-theatre/listen/the-hiding-place-part-02-of-09'
+    '-792545.html')
 RT.refresh()
 feeds.append(RT)
 
 page = 'https://www.oneplace.com/ministries/turning-point/'
 title = 'Turning Point'
-desc = "Dr. David Jeremiah's Turning Point has a Mission: Delivering the Unchanging Word of God to an Ever-Changing " \
-       "World "
-id = page
-TP = OneFeed.OnePlacePodCast(page, title, desc, id)
+desc = 'Dr. David Jeremiah\'s Turning Point Delivers the Unchanging Word of God to an Ever-Changing World'
+feed_id = page
+author_dict = {'name': 'David Jeremiah', 'email': 'info@davidjeremiah.org'}
+TP = OneFeed.OnePlacePodCast(page, title, desc, feed_id)
+TP.author(author_dict)
 TP.limit = 30
 TP.refresh()
 feeds.append(TP)
--- FeedsUpdate.py
+++ FeedsUpdate.py
@@ -1,5 +1,19 @@
+import pickle
+
 # load the pickled list of feeds
+with open('feeds.p', 'rb') as file:
+    feeds = pickle.load(file)
 
 # call the refresh of each feed and then write the rss feed
+with open('update.log', 'wb') as log:
+    for feed in feeds:
+        try:
+            ep = feed.refresh()
+        except:
+            log.write(f'exception updating {feed.title()}')
+        if ep is None:
+            log.write(f'No new show for {feed.title()}')
+        else log.write(f'{ep.title()} added to {feed.title()}')
 
-#commit the docs folder and push to github
+    #the refresh wil download any flagged as such.
+# commit the docs folder and push to github
