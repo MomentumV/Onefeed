fc81402fa54178f8503e4499a0342843045ac13c
MomentumV <momentumv@users.noreply.github.com>
2020-01-20 14:34:52 -0500

    Read feeds from pickled file.

--- FeedsUpdate.py
+++ FeedsUpdate.py
@@ -1,6 +1,8 @@
 
 import pickle
 import git_code
+with open('feeds.p', 'rb') as f:
+    feeds = pickle.load(f)
 
 # call the refresh of each feed and then write the rss feed
 with open('update.log', 'w') as log:
@@ -17,5 +19,8 @@ with open('update.log', 'w') as log:
             feed.rss_file()
 
     # the refresh wil download any flagged as such.
+# re-pickle the feeds file:
+with open('feeds.p', 'wb') as file:
+    pickle.dump(feeds, file)
 # commit the project folder and push to github
 git_code.git_push()
