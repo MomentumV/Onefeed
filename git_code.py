bf0233d44f4f752a233833bf3374b6ae301b8d08
MomentumV <momentumv@users.noreply.github.com>
2020-02-13 12:37:08 -0500

    update script driven git commit message to include timestamp

--- git_code.py
+++ git_code.py
@@ -1,14 +1,14 @@
 from git import Repo
 
 PATH_OF_GIT_REPO = r'C:\Users\morde\PycharmProjects\Onefeed\.git'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = 'update from script'
+COMMIT_MESSAGE = 'update from script '
 
 
-def git_push():
+def git_push(message=COMMIT_MESSAGE):
     try:
         repo = Repo(PATH_OF_GIT_REPO)
         repo.git.add(A=True)
-        repo.index.commit(COMMIT_MESSAGE)
+        repo.index.commit(message)
         origin = repo.remote(name='origin')
         origin.push()
         repo.git.clear_cache()
